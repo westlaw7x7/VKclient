f9af4184be4c7c6f9635baa50cbde1f1b76e983d
Alexander <a.a.grigoryev@icloud.com>
2022-07-01 18:33:28 +0300

    Commented code was deleted

--- VKclient/App/Entity/GroupNewsModel.swift
+++ VKclient/App/Entity/GroupNewsModel.swift
@@ -35,27 +35,3 @@ extension Community: Codable {
         case photo = "photo_100"
     }
 }
-
-//class GroupNews: NewsSource {
-//    var urlString: String { photo100 }
-//    var pictureUrl: URL? { URL(string: photo100) }
-//
-//
-//    var id: Int = 0
-//    var name: String = ""
-//    var photo100: String = ""
-//
-//    required convenience init(_ json: JSON) {
-//        self.init()
-//        self.id = json["id"].intValue
-//        self.name = json["name"].stringValue
-//        self.photo100 = json["photo_100"].stringValue
-//    }
-//
-//    convenience init(id: Int, name: String, photo100: String) {
-//        self.init()
-//        self.id = id
-//        self.name = name
-//        self.photo100 = photo100
-//    }
-//}
--- VKclient/App/Entity/UserNewsModel.swift
+++ VKclient/App/Entity/UserNewsModel.swift
@@ -32,31 +32,3 @@ struct User: Codable, NewsSource {
         case avatar = "photo_100"
     }
 }
-
-//class UserNews: NewsSource {
-//
-//    var name: String { "\(firstName) \(lastName)" }
-//
-//    var urlString: String { photo100 }
-//
-//    var id: Int = 0
-//    var firstName: String = ""
-//    var lastName: String = ""
-//    var photo100: String = ""
-//
-//    required convenience init(_ json: JSON) {
-//        self.init()
-//        self.id = json["id"].intValue
-//        self.firstName = json["first_name"].stringValue
-//        self.lastName = json["last_name"].stringValue
-//        self.photo100 = json["photo_100"].stringValue
-//    }
-//
-//    convenience init(id: Int, firstName: String, lastName: String, photo100: String) {
-//        self.init()
-//        self.id = id
-//        self.firstName = firstName
-//        self.lastName = lastName
-//        self.photo100 = photo100
-//    }
-//}
